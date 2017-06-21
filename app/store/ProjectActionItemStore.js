4f867e4dc36570a837612d110865719129736334
Luis Cevallos <lcevallo@gmail.com>
2017-06-21 09:43:42 -0500

    Este ejemplo es bueno para MVC

---
+++ app/store/ProjectActionItemStore.js
@@ -0,0 +1,12 @@
+﻿Ext.define("Chapter09.store.ProjectActionItemStore", {
+    extend: "Ext.data.Store",
+    model: "Chapter09.model.ProjectActionItem",
+    proxy: {
+        type: "ajax",
+        url: "actionitems.txt",
+        reader: {
+            type : "json",
+            root : "actionitems"
+        }
+    }
+});
